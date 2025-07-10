2bed980d8ceafa5bfbb7879ae844b9e3b6600341
Tsvetan Kintisheff <kintisheff@gmail.com>
2025-07-10 21:33:15 +0200

    setup the spark-tts python package

---
+++ tests/test_entrypoint.py
@@ -0,0 +1,7 @@
+"""Provides a unit test for the accessibility of the main entrypoint to the package."""
+# ruff: noqa: ANN201, F401
+
+
+def test_entrypoint():
+    """Asserts a successful import of the main package entrypoint."""
+    import spark_tts
